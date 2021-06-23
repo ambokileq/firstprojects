ce394d35c94936cc08387d21141e79c70339bb39
ambokileq <86386136+ambokileq@users.noreply.github.com>
2021-06-24 02:54:49 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>responsive</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script type="text/javascript" src="assets/js/style.js"></script>
+</head>
+
+<body>
+
+
+	<header>
+		<div class="toggle-btn" onclick="myFunction();">
+			<span></span>
+			<span></span>
+			<span></span>
+			
+		</div>
+		<div class="logo">
+			<h1>LOGO</h1>
+			
+		</div>
+		<div class="profile">
+			<h2>mypfofile</h2>
+			
+		</div>
+		<nav id="nav">
+			<ul>
+				<li><a class="active" href="#">home</a></li>
+				<li><a class="active" href="#">about</a></li>
+				<li><a class="active" href="#">services</a></li>
+				<li><a class="active" href="#">gallary</a></li>
+				<li><a class="active" href="#">contact</a></li>
+			</ul>
+		</nav>
+	</header>
+
+</body>
+</html>
